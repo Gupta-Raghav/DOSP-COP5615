4b2c20703bc8411954f3f11f2bc422e291e46948
Gupta-Raghav <42888633+Gupta-Raghav@users.noreply.github.com>
2022-10-14 21:57:20 -0400

    started projest 3.

---
+++ Project-3/node.erl
@@ -0,0 +1,7 @@
+-module(node).
+
+node(ID,M,Existing_Nodes)->
+    fingertable, id , pre, suc,m. %for now our Node will handle all this. 
+
+ft_node()->
+        mathfingertablecalculation. %pow(id+pow(2,m)).   
--- Project-3/project-3.erl
+++ Project-3/project-3.erl
@@ -1,6 +1,35 @@
 -module(project3).
 
--export([start/0]).
+-export([start/2]).
 
-start()->
+create_chord(0,_) ->
+    list;
+
+create_chord(NNodes,Nreq)->
+    addNode(NNodes,[]).
+
+start(NumNodes,NumRequests)->
+    M = eralng:trunc(ceil(math:log(2,NumNodes))),
+    Chordspace = math:pow(2,M),
+    addNode(Chordspace,[]),
+    create_chord(NumRequests,Chordspace-1),
     ok.
+
+addNode(N,NodeList)->
+    Node = spawn(node, start, []),
+    Hashval = crypto:hash(sha,Node),
+    addNode(N- 1, [Hashval | NodeList]),
+    adding.
+
+deletingNode()->
+    deleting.
+
+stabalize()->
+    stabalize.
+
+ask()->
+    ask.
+
+hashing(Val)->
+   Hash = crypto:hash(sha,Val),
+   Hash.
