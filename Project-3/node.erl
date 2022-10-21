8ad03594f3421adbc95e5b091f56c80ea9f29eed
Gupta-Raghav <42888633+Gupta-Raghav@users.noreply.github.com>
2022-10-20 22:37:42 -0400

    [SOLVED]:Creating the chord,[UNSOLVED]:assigning keys

--- Project-3/node.erl
+++ Project-3/node.erl
@@ -1,7 +1,38 @@
 -module(node).
+-export([start/7]).
 
-node(ID,M,Existing_Nodes)->
-    fingertable, id , pre, suc,m. %for now our Node will handle all this. 
+start(ID,M,Existing_Nodes,No,Ft,Sc,Pr)->
+    % io:format("Node with Id ~p Existing Nodes ~p and value of M ~p~n",[ID,Existing_Nodes,M]),
 
-ft_node()->
-        mathfingertablecalculation. %pow(id+pow(2,m)).   
+    % io:format("Suc: ~p,Pre: ~p~n",[Sc,Pr]),
+    receive
+        {createFt,NodeList}->
+            FingerTable = ft_node(1,M,ID,NodeList,No,Ft),
+            listener ! {FingerTable};
+        {updateSP,S,P} ->
+            start(ID,M,Existing_Nodes,No,Ft,S,P);
+        {populate}->
+            % populateKeys(),
+            listener ! done_populating
+
+
+
+    end.
+    % Keys = lists:seq(P, ID).
+
+
+    
+ft_node(I,Ftm,_,_,_,Ft) when I ==Ftm+1->
+    Ft;
+
+ft_node(I,FtM,Id, Nodes,No,Ft)->
+    
+        X = erlang:trunc(Id + math:pow(2,I-1)),
+        Snode = (X) rem (No), 
+        if
+            Snode ==0 ->
+                   ft_node(I+1,FtM,Id,Nodes,No,[X|Ft]);
+            true ->
+                ft_node(I+1,FtM,Id,Nodes,No,[Snode|Ft])
+        end.
+        
---
+++ Project-3/project3.erl
@@ -0,0 +1,75 @@
+-module(project3).
+
+-export([start/2,listener/0]).
+
+iterator(0,Ms)->
+    Ms;
+
+iterator(N,M)->
+    iterator(N bsr 1,M+1).
+
+
+assign_keys(M,List) ->
+        lists:foreach(fun({Id,Node})->
+            Node ! {keys, lists:seq()}
+            end,List),
+        g.
+    
+listener() ->
+    receive
+        {KeyMap} ->
+            io:format("Finger Table ~p~n",[KeyMap]),
+            listener()
+    end.
+
+
+start(NumNodes,NumRequests)->
+    M = 5,
+    io:format("Nodes to be Checked: ~p~n",[NumNodes]),
+    io:format("Requests to be checked: ~p~n",[NumRequests]),
+    io:format("Finger tuple contains: ~p~n tuples",[M]),
+    register(listener, spawn(project3, listener, [])),
+    FT = maps:new(),
+    create_chord(NumNodes,4,[],NumRequests,[], FT).
+
+create_chord(0,M,NodeList,_,_, FT)->
+    % io:format("Id list~p~n",[IdList]),
+    NList = lists:keysort(1,NodeList),
+    io:format("Node list sorted~p~n",[NList]),
+    lists:foreach(fun({Id,Node})->
+                    Ind = string:str(NList, [{Id,Node}]),
+                    if
+                        Ind ==1 ->
+                            Node ! {updateSP, lists:nth(Ind + 1, NList),lists:nth(length(NList), NList)};
+                        Ind ==length(NList) ->
+                            Node ! {updateSP, lists:nth(1, NList),lists:nth(Ind - 1, NList)};
+                        true ->
+                            Node ! {updateSP, lists:nth(Ind + 1, NList),lists:nth(Ind-1, NList)}
+                    end
+                    end,NList),
+    assign_keys(M,NList);     
+    % assign keys.
+    % Ft creation.
+    % NodeList;
+
+create_chord(N,M,NodeList,Nreq,IdList,FT)->
+    Id= rand:uniform(round(math:pow(2,M))),
+    Flag = lists:member(Id,IdList),
+    if
+        Flag ->
+            create_chord(N,M,NodeList,Nreq,IdList,FT);
+        true -> 
+            Node = spawn(node, start, [Id,M,NodeList,N,[],0,0]),
+            NodeId = {Id,Node},
+            create_chord(N-1,M,[NodeId|NodeList],Nreq,[Id|IdList],FT)
+    end.
+    
+
+stabalize()->
+    find_successor(),
+    stabalize.
+
+find_successor()->
+    ask.
+
+
