35e6178b939377492b17c2b51b814ba8a8218ce8
Gupta-Raghav <42888633+Gupta-Raghav@users.noreply.github.com>
2022-10-21 20:18:44 -0400

    [SOLVED]:finger table creation and stabalization

--- Project-3/node.erl
+++ Project-3/node.erl
@@ -6,36 +6,57 @@ start(ID,M,Existing_Nodes,No,Ft,Sc,Pr)->
 
     % io:format("Suc: ~p,Pre: ~p~n",[Sc,Pr]),
     receive
+        {lookup,K}->
+            search(K,ID,Sc,Ft,0);
         {createFt,NodeList}->
             FingerTable = ft_node(1,M,ID,NodeList,No,Ft,Sc,Pr),
-            start(ID,M,Existing_Nodes,No,Ft,Sc,Pr),
-            listener ! {FingerTable};
+            listener ! {fingertablecreated},
+            start(ID,M,Existing_Nodes,No,Ft,Sc,Pr);
         {updateSP,S,P} ->
-            start(ID,M,Existing_Nodes,No,Ft,S,P),
-            listener ! {doneupdating};
+            listener ! {doneupdating},
+            start(ID,M,Existing_Nodes,No,Ft,S,P);
         {populate}->
             % populateKeys(),
             listener ! done_populating;
         {askSucessor,Sk,PID} ->
-            {SNode,SId} = Sc,
+            {SId,SNode} = Sc,
             if
                 Sk =< SId ->
+
                     PID ! {Sc};
-                true ->
-                    SNode ! {askSucessor,Sk,PID}
+                true -> 
+                    %  io:format("inside the false statement~n"),
+                     SNode ! {askSucessor,Sk,PID}
             end, 
             start(ID,M,Existing_Nodes,No,Ft,Sc,Pr)
     end.
-    % Keys = lists:seq(P, ID).
+
+% binary search
+% middle element
+% low , high
+nearest(Key,Fingert,It)->
+    nearest.
+
+
+search(Skey,Id,Scc={SID,SPid},Ft,Hops)->
+    if
+        Skey > Id, Skey=<SID ->
+            listener ! {found};
+        Id > SID, Skey=<SID ->
+            listener ! {found};
+        Id > SID, Skey > SID ->
+            listener ! {found}; 
+        true ->
+            nearest(Skey,Ft,length(Ft))
+    end.
 
 
     
 ft_node(I,Ftm,ID,_,_,Ft,_,_) when I ==Ftm+1->
-    % io:format("Finger Table for Id ~p is ~p~n",[ID,Ft]),
+    io:format("Finger Table for Id ~p is ~p~n",[ID,Ft]),
     Ft;
 
 ft_node(I,FtM,Id, Nodes,No,Ft,S,P)->
-    
         Key = erlang:trunc(Id + math:pow(2,I-1)),
         SKey = (Key+1) rem round(math:pow(2,FtM)),
         % io:format("Calculating for ID ~p \t Key ~p, Skey: ~p~n",[Id,Key,SKey]),
@@ -50,6 +71,7 @@ ft_node(I,FtM,Id, Nodes,No,Ft,S,P)->
                 SNode ! {askSucessor,SKey,self()},
                 receive
                     {Sucessor}->
+                        % io:format("Successor ~p~n",[Sucessor]),
                         ft_node(I+1,FtM,Id,Nodes,No,[{SKey,Sucessor}|Ft],S,P)
                 end 
         end.
--- Project-3/project3.erl
+++ Project-3/project3.erl
@@ -1,40 +1,53 @@
 -module(project3).
 
--export([start/2,listener/0]).
+-export([start/2,listener/2]).
 
-iterator(0,Ms)->
-    Ms;
+% iterator(0,Ms)->
+%     Ms;
 
-iterator(N,M)->
-    iterator(N bsr 1,M+1).
+% iterator(N,M)->
+%     iterator(N bsr 1,M+1).
 
 
-assign_keys(M,List) ->
-        lists:foreach(fun({Id,Node})->
-            Node ! {keys, lists:seq()}
-            end,List),
-        g.
+% assign_keys(M,List) ->
+%         lists:foreach(fun({Id,Node})->
+%             Node ! {keys, lists:seq()}
+%             end,List),
+%         g.
     
-listener() ->
+lookup(List)->
+    {_,Node} = lists:nth(rand:uniform(length(List)), List),
+    {Key,_} = lists:nth(rand:uniform(length(List)), List),
+    Node ! {lookup,Key}.
+
+listener(0,List)->
+    % Start lookup after this
+    io:format("starting lookup after this\n"),    
+    lookup(List),
+    listener(length(List),List),
+    ok;
+
+listener(N,NodeList) ->
     receive
-        {KeyMap} ->
-            io:format("Finger Table ~p~n",[KeyMap]),
-            listener()
+        {fingertablecreated} ->
+            listener(N-1,NodeList);
+        {Hops}->
+            ok
     end.
 
 
 start(NumNodes,NumRequests)->
-    M = 3,
+    M = 5,
     io:format("Nodes to be Checked: ~p~n",[NumNodes]),
     io:format("Requests to be checked: ~p~n",[NumRequests]),
     io:format("Finger tuple contains: ~p~n tuples",[M]),
-    register(listener, spawn(project3, listener, [])),
+    register(listener, spawn(project3, listener, [NumNodes,[]])),
     create_chord(NumNodes,M,[],NumRequests,[]).
 
 create_chord(0,M,NodeList,_,_)->
     % io:format("Id list~p~n",[IdList]),
     NList = lists:keysort(1,NodeList),
-    io:format("Node list sorted~p~n",[NList]),
+    % io:format("Node list sorted~p~n",[NList]),
     lists:foreach(fun({Id,Node})->
                     Ind = string:str(NList, [{Id,Node}]),
                     if
@@ -46,6 +59,8 @@ create_chord(0,M,NodeList,_,_)->
                             Node ! {updateSP, lists:nth(Ind + 1, NList),lists:nth(Ind-1, NList)}
                     end
                     end,NList),
+    N = length(NList),
+    listener ! {N,NList},
     lists:foreach(fun({_,Node})->
                 Node ! {createFt,NList}    
                 end,NList);
@@ -67,11 +82,11 @@ create_chord(N,M,NodeList,Nreq,IdList)->
     end.
     
 
-stabalize()->
-    find_successor(),
-    stabalize.
+% stabalize()->
+%     find_successor(),
+%     stabalize.
 
-find_successor()->
-    ask.
+% find_successor()->
+%     ask.
 
 
