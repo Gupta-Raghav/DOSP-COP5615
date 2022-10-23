c7ada64903784f5739d8ab64426ccda0cca500f8
Gupta-Raghav <42888633+Gupta-Raghav@users.noreply.github.com>
2022-10-22 23:00:50 -0400

    just works sometimes

--- Project-3/node.erl
+++ Project-3/node.erl
@@ -8,176 +8,136 @@ start(ID,M,Existing_Nodes,No,Ft,Sc,Pr)->
     receive
         {lookup,K}->
             % io:format("Inside Lookup\n"),
-            search(K,ID,Sc,Ft,0);
+            % io:format("here\n"),
+            search(K,ID,Sc,Ft,0,round(math:pow(2,M))),
+            % io:format("Id ~p Key ~p~n",[ID,K]),
+            start(ID,M,Existing_Nodes,No,Ft,Sc,Pr);
         {continuesearch, K, Hop} ->
             % io:format("IN continue with hops : ~p~n",[Hop]),
-            search(K,ID,Sc,Ft,Hop);
+            search(K,ID,Sc,Ft,Hop,round(math:pow(2,M))),
+            start(ID,M,Existing_Nodes,No,Ft,Sc,Pr);
         {createFt,NodeList}->
-            %io:format("Inside createft\n"),
+            % io:format("Inside createft\n"),
             FingerTable = ft_node(1,M,ID,NodeList,No,Ft,Sc,Pr),
-            %io:format("ft done: ~p~n",[FingerTable]),
+            io:format("ID ~p ft done: ~p~n",[ID,FingerTable]),
             listener ! {fingertablecreated},
             start(ID,M,Existing_Nodes,No,FingerTable,Sc,Pr);
         {updateSP,S,P} ->
             listener ! {doneupdating},
             start(ID,M,Existing_Nodes,No,Ft,S,P);
-        {populate}->
-            % populateKeys(),
-            listener ! done_populating;
         {askSucessor,Sk,PID} ->
-            % {SId,SNode} = Sc,
-            % if
-            %     Sk =< SId ->
-
-            %         PID ! {Sc};
-            %     true -> 
-            %         %  io:format("inside the false statement~n"),
-            %          SNode ! {askSucessor,Sk,PID}
-            % end, 
-            %io:format("about to call contft"),
             contFT(Sk, PID, Sc, ID, M),
             start(ID,M,Existing_Nodes,No,Ft,Sc,Pr)
     end.
-nearest(Key,Fingert,1,PID)->
-    {_,Succ} = lists:nth(1, Fingert),
-    PID ! Succ;
-
-nearest(Key,Fingert,It,PID)->
-    {_,Succ} = lists:nth(It, Fingert),
-    {Fid,_} = Succ,
+nearest(ID,Key,Fingert,It,PID,Space)->
     if
-        Fid < Key ->
-            % io:format("here in the true conditon~n"),
-           PID ! {Succ};
-        true ->
-            % io:format("here in the False conditon~n"),
-            nearest(Key,Fingert,It-1,PID)
-    end.
-
-
-search(Skey,Id,Scc={SID,_},Ft,Hops)->
-    % io:format("Inside Search ~n"),
-    if
-        Skey > Id, Skey=<SID ->
-            % io:format("First condition ~n"),
-            listener ! {found,Scc,Skey,Hops+1};
-        Id > SID, Skey=<SID ->
-            % io:format("Second condition ~n"),
-            listener ! {found,Scc,Skey,Hops+1};
-        Id > SID, Skey > SID ->
-            % io:format("Third condition ~n"),
-            listener ! {found,Scc,Skey,Hops+1}; 
-        true ->
-            % io:format("False condition ~n"),
-            nearest(Skey,Ft,length(Ft),self()),
-            receive
-                {{_,SUCCPID}}->
-                % io:format("SUCCPID ~p\n",[SUCCPID]),
-                SUCCPID ! {continuesearch, Skey, Hops+1}
-                % io:format("Found S through Nearest ~p~n",[S])
-            end
-            
-    end.
-
-
-    
-ft_node(I,Ftm,ID,_,_,Ft,_,_) when I ==Ftm+1->
-     io:format("Finger Table for Id ~p is ~p~n",[ID,Ft]),
-    Ft;
-
-ft_node(I,FtM,Id, Nodes,No,Ft,S,P)->
-        % io:format("here in the Finger table conditon~n"),
-        Key = round(Id + round(math:pow(2,I-1))),
-        SKey = (Key) rem round(math:pow(2,FtM)),
-        %io:format("Calculating for ID ~p \t Key ~p, Skey: ~p~n",[Id,Key,SKey]),
-        {SId ,SNode} = S,
-        {PId ,PNode} = P,
-        %io:format("Above  SId:~p, Skey: ~p~n",[SId,SKey]),
-        % if
-        %     I > SId, I > Key, SId >= Key ->
-        %     % io:fwrite("Cond2~n", []),
-        %     Succ;
-        %      I > SId, I < Key, SId =< Key ->
-        %     % io:fwrite("Cond6~n", []),
-        %     Succ;
-        %     Key > I, Key =< SId ->
-        %     % io:fwrite("Cond3~n", []),
-        %     Succ;
-        NameSpace = round(math:pow(2,FtM)),
-        %numbers between last and first node
-        Temp = lists:member(SKey, lists:seq(Id, NameSpace)++lists:seq(0, SId)),
-        if
-            SKey=< SId, SKey > Id->
-                %io:format("Inserting ~p, ~p: in FT of : ~p  ~n",[SKey,S, Id]),
-                ft_node(I+1,FtM,Id,Nodes,No,[{SKey,S}|Ft],S,P);
-
-            SId < Id ->
-                if
-                    Temp ->
-                       % io:format("Inserting ~p, ~p: in FT of : ~p  ~n",[SKey,S, Id]),
-                        ft_node(I+1,FtM,Id,Nodes,No,[{SKey,S}|Ft],S,P);
-                    true ->
-                        SNode ! {askSucessor,SKey,self()}
-                end;
-                
-
-            SKey=< SId, SKey < Id; 
-            SKey > SId->
-                %io:format("Below SId:~p, Skey: ~p~n",[SId,SKey]),
-                SNode ! {askSucessor,SKey,self()},
-                receive
-                    {Sucessor}->
-                        %io:format("FT entry received ~p~n",[Sucessor]),
-                        ft_node(I+1,FtM,Id,Nodes,No,[{SKey,Sucessor}|Ft],S,P)
-                end 
-        end.
-        
-contFT(Sk, ReqID, Sc, Id, M) ->
-    
-    {SId,SNode} = Sc,
-    NameSpace = round(math:pow(2,M)),
-    %io:format("in contFT "),
-    Temp = lists:member(Sk, lists:seq(Id, NameSpace)++lists:seq(0, SId)),
-
-    if
-        Sk=< SId, Sk > Id->
-            %io:format("Inserting ~p: in FT of : ~p  ~n",[Sc, Id]),
-            ReqID ! {Sc};
-
-        SId < Id ->
+        It ==0->
+            {_,Succ} = lists:last(Fingert),
+            Succ;
+        true->
+            {_, Finger={LId, _LPid}} = lists:nth(It,Fingert),
             if
-                    Temp ->
-                        
-                        ReqID ! {Sc};
-                    true ->
-                        SNode ! {askSucessor,Sk,ReqID}
-                end;
-
-        Sk=< SId, Sk < Id; 
-        Sk > SId->
-            SNode ! {askSucessor,Sk,ReqID}
+                Key < ID->
+                    io:format("here\n"),
+                    {_,Succ} = lists:last(Fingert),
+                    Finger;
+                ID <LId, LId<Key ->
+                    Finger;
+                true ->
+                    nearest(ID,Key,Fingert,It-1,PID,Space)  
+            end
     end.
-    
-    % if 
-    %     %%Normal clockwise circulation
-    %     SId >= Id ->
-    %        % io:format("line 131 "),
-    %         if
-    %             Sk =< SId, Sk > Id ->
-    %                 io:format("line 134~n"),
-    %                 ReqID ! {Sc};
-    %             Sk=< SId, Sk < Id; 
-    %             Sk > SId->
-    %                 SNode ! {askSucessor,Sk,ReqID}
-    %         end;
-    %     SId < Id ->
-    %         if
-    %             Sk =< SId ->
-    %                 io:format("found successor after circling ring ~p~n",[Sc]),
-    %                 ReqID ! {Sc};
-    %             Sk > SId ->
-    %                 SNode ! {askSucessor,Sk,ReqID}
-    %         end
-    % end.
+
+
+search(Skey,Id,Scc={SID,_},Ft,Hops,Space)->
+    io:format("Inside Id ~p, Finger Table ~p Searching ~p~n",[Id,Ft,Skey]),
+    if
+        Id =<Skey ->
+            lookupListener ! {found,Id,{Id, self()},Skey,Hops+1};
+        Id > SID, Skey =< Id ->
+            lookupListener ! {found,Id,Scc,Skey,Hops+1};
+        Id > SID, Skey > Id ->
+            lookupListener ! {found,Id,Scc,Skey,Hops+1};
+        Skey > Id, Skey=<SID ->
+            lookuupListener ! {found,Id,Scc,Skey,Hops+1};
+        % Skey > Id, Skey > SID ->
+        %     lookupListener ! {found,Id,Scc,Skey,Hops+1};
+        true ->
+            {_,SUCCPID} =  nearest(Id,Skey,Ft,length(Ft),self(),Space),
+            % io:format("Succesor PID~p~n",[SUCCPID]),
+            SUCCPID ! {continuesearch, Skey, Hops+1}
+    end.
+
 
     
+    ft_node(I,Ftm,ID,_,_,Ft,_,_) when I ==Ftm+1->
+        FT = lists:reverse(Ft),
+    %    io:format("Finger Table for Id ~p is ~p~n",[ID,FT]),
+       FT;
+   
+   ft_node(I,FtM,Id, Nodes,No,Ft,S,P)->
+           % io:format("here in the Finger table conditon~n"),
+           Key = round(Id + round(math:pow(2,I-1))),
+           SKey = (Key) rem round(math:pow(2,FtM)),
+           %io:format("Calculating for ID ~p \t Key ~p, Skey: ~p~n",[Id,Key,SKey]),
+           {SId ,SNode} = S,
+           NameSpace = round(math:pow(2,FtM)),
+           %numbers between last and first node
+           Temp = lists:member(SKey, lists:seq(Id+1, NameSpace)++lists:seq(0, SId)),
+        %    io:format("Inserting ~p, ~p: in FT of : ~p asking node ~p ~n",[SKey,S, Id,SId]),
+           if
+               SKey=< SId, SKey > Id->
+                   %io:format("Inserting ~p, ~p: in FT of : ~p  ~n",[SKey,S, Id]),
+                   ft_node(I+1,FtM,Id,Nodes,No,[{SKey,S}|Ft],S,P);
+   
+               SId < Id ->
+                   if
+                       Temp ->
+                           ft_node(I+1,FtM,Id,Nodes,No,[{SKey,S}|Ft],S,P);
+                       true ->
+                           SNode ! {askSucessor,SKey,self()},
+                           receive
+                           {Sucessor}->
+                           %io:format("FT entry received ~p~n",[Sucessor]),
+                           ft_node(I+1,FtM,Id,Nodes,No,[{SKey,Sucessor}|Ft],S,P)
+                   end
+
+                   end;
+                   
+   
+               SKey=< SId, SKey < Id; 
+               SKey > SId->
+                   %io:format("Below SId:~p, Skey: ~p~n",[SId,SKey]),
+                   SNode ! {askSucessor,SKey,self()},
+                   receive
+                       {Sucessor}->
+                           %io:format("FT entry received ~p~n",[Sucessor]),
+                           ft_node(I+1,FtM,Id,Nodes,No,[{SKey,Sucessor}|Ft],S,P)
+                   end 
+           end.
+           
+   contFT(Sk, ReqID, Sc, Id, M) ->
+       
+       {SId,SNode} = Sc,
+       NameSpace = round(math:pow(2,M)),
+    %    io:format("in contFT "),
+       Temp = lists:member(Sk, lists:seq(Id, NameSpace)++lists:seq(0, SId)),
+    %    io:format("Inserting ~p, ~p: in FT of : ~p asking node ~p ~n",[SKey,S, Id,SId]),
+       if
+           Sk=< SId, Sk > Id->
+               %io:format("Inserting ~p: in FT of : ~p  ~n",[Sc, Id]),
+               ReqID ! {Sc};
+   
+           SId < Id ->
+               if
+                       Temp ->
+                           
+                           ReqID ! {Sc};
+                       true ->
+                           SNode ! {askSucessor,Sk,ReqID}
+                   end;
+   
+           Sk=< SId, Sk < Id; 
+           Sk > SId->
+               SNode ! {askSucessor,Sk,ReqID}
+       end.
--- Project-3/project3.erl
+++ Project-3/project3.erl
@@ -1,29 +1,47 @@
 -module(project3).
+-compile(export_all).
+-export([start/1,listener/4,lookupListener/3]).
 
--export([start/2,listener/2]).
 
-lookup(List)->
-    {_,Node} = lists:nth(1, List),
-    {Key,_} = lists:nth(rand:uniform(length(List)), List),
-    % io:format("Asking node ~p with key~p ~n",[Node,Key]),
-    Node ! {lookup,Key}.
+% iterate the node list to start doing the lookup.
+% everytime it finds something it replies back to the listener.
+% listener wait for all the numNodes * numRequests number of messages and then exits after printing the 
+% the average of the hops.
+lookup(_,0,_)->
+    ok;
 
-listener(N,List) when N ==0->
-    % Start lookup after this
-    io:format("starting lookup after this\n"),   
-    lookup(List),
-    %io:format("FT successfully filled"),
+lookup(M,N,List)->
+    Key = rand:uniform(round(math:pow(2,10)))-1,
+    lists:foreach(fun({_,Node})->
+                % io:format("sending ~p Node ~p time~n",[N,Node]),
+                Node ! {lookup,Key}
+                end,List),
+    lookup(M,N-1,List).
+
+lookupListener(Total,AvgH,Div) when Total ==0->
+    io:format("Average hops = ~p~n",[AvgH/Div]);
+
+lookupListener(Total,AvgH,Div)->
     receive
-        {found,FNode,Key,H}->
-            io:format("Hop on Fnode ~p with key~p : ~p~n",[FNode,Key,H])
-    end,
-    listener(0,List);
+        {found,Id,Scc,Skey,Hops} ->
+            % io:format("Lookup listener count ~p\n",[Total]), 
+            % io:format("Found  ~p  at node ~p  starting from node ~p~n",[Skey, Scc, Id]),
+            lookupListener(Total-1,AvgH+Hops,Div)
+    end.
 
 
-listener(N,NodeList) ->
+listener(M,N,List,NumRequests) when N ==0->
+    % io:format("starting lookup.\n"),
+    
+    % lookupListener(T,Hops,T),
+    % lookup(List);
+    lookup(M,NumRequests,List);
+
+
+listener(M,N,NodeList,NumRequests) ->
     receive
         {Num,List}->
-            io:format("received nlist~p~n",[List]),
+            % io:format("received nlist~p~n",[List]),
             lists:foreach(fun({_,Node})->
                 Node ! {createFt,List},  
                 receive
@@ -31,16 +49,20 @@ listener(N,NodeList) ->
                         ok
                 end
                 end,List),
-            listener(0,List)
+            listener(M,0,List,NumRequests)
     end.
 
 
-start(NumNodes,NumRequests)->
+start([_NumNodes,_NumRequests])->
+    NumNodes = list_to_integer(_NumNodes),
+    NumRequests = list_to_integer(_NumRequests),
     M = 5,
     io:format("Nodes to be Checked: ~p~n",[NumNodes]),
     io:format("Requests to be checked: ~p~n",[NumRequests]),
     io:format("Finger tuple contains: ~p~n tuples",[M]),
-    register(listener, spawn(project3, listener, [NumNodes,[]])),
+    register(listener, spawn(project3, listener, [M,NumNodes,[],NumRequests])),
+    T = NumRequests*NumNodes,
+    register(lookupListener, spawn(project3, lookupListener, [T,0,T])),
     create_chord(NumNodes,M,[],NumRequests,[]).
 
 create_chord(0,_,NodeList,_,_)->
@@ -86,4 +108,13 @@ create_chord(N,M,NodeList,Nreq,IdList)->
 % find_successor()->
 %     ask.
 
+lookup(Nodelist)->
+    receive
+        {lookup,Key} ->
+            {_,Node} =lists:nth(1, Nodelist),
+            Node ! {lookup,Key},
+            lookup(Nodelist)
+    end.
 
+z(Key)->
+    listener ! {lookup, Key}.
